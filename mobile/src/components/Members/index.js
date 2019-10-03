558002fa5d5861920541507010ee722b035cf095
Hugo <hugohoffmann04@gmail.com>
2019-10-03 10:56:27 -0300

    new: members invite

--- mobile/src/components/Members/index.js
+++ mobile/src/components/Members/index.js
@@ -6,16 +6,30 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 import MembersActions from '~/store/ducks/members';
+import InviteMember from '~/components/InviteMember';
 import styles from './styles';
 
 class Members extends Component {
-    componentDidMount(){
-      const { getMembersRequest } = this.props;
+  state = {
+    isIviteModalOpen: false,
+  };
 
-      getMembersRequest();
-    }
+  toggleInviteModalOpen = () => {
+    this.setState({ isIviteModalOpen: true });
+  }
+
+  toggleInviteModalClosed = () => {
+    this.setState({ isIviteModalOpen: false });
+  }
+
+  componentDidMount() {
+    const { getMembersRequest } = this.props;
+
+    getMembersRequest();
+  }
   render() {
     const { members } = this.props;
+    const { isIviteModalOpen } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
@@ -23,25 +37,29 @@ class Members extends Component {
         </Text>
         <FlatList
           style={styles.memberList}
-          data={ members.data }
-          keyExtractor={ item => String(item.id)}
-          renderItem={({item}) => (
+          data={members.data}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
             <View style={styles.memberContainer}>
               <Text style={styles.memberName}>
-                { item.user.name }
+                {item.user.name}
               </Text>
-            <TouchableOpacity hitSlop={{ top: 5,bottom: 5, left: 5, right: 5 }} onPress={() => {}}>
-              <Icon name="settings" size={20} color="#b0b0b0" />
-            </TouchableOpacity>
+              <TouchableOpacity hitSlop={{ top: 5, bottom: 5, left: 5, right: 5 }} onPress={() => { }}>
+                <Icon name="settings" size={20} color="#b0b0b0" />
+              </TouchableOpacity>
             </View>
           )}
           ListFooterComponent={() => (
-            <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <TouchableOpacity style={styles.button} onPress={this.toggleInviteModalOpen}>
               <Text style={styles.buttonText}>
                 Convidar
               </Text>
             </TouchableOpacity>
           )}
+        />
+        <InviteMember
+          visible={isIviteModalOpen}
+          onRequestClose={this.toggleInviteModalClosed}
         />
       </View>
     );
